51b66426990dc7cc901d59296508889f0435f085
rfodge <reedfodge@gmail.com>
2021-03-05 19:40:50 -0500

    Added style changes

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
-const endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"; // Adding array to a const variable 
+const endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"; // Adding array to a const variable
 
-// Empty array to put our restaurant names into 
+// Empty array to put our restaurant names into
 const names=[];
 
 // Using api fetch. Looking into Async/await as a replacement later
@@ -9,32 +9,33 @@ fetch(endpoint)
 	.then(data => names.push(...data))
 
 // Run a function to show everything that matches the word being typed in the box
-// The function is going to take this huge array and filter it down into a subset 
+// The function is going to take this huge array and filter it down into a subset
 function findMatches(wordToMatch, names) {
 	return names.filter(restaurants => {
 		const regex = new RegExp (wordToMatch, 'gi');
-		return restaurants.name.match(regex) 		
+		return restaurants.name.match(regex)
 
 	});
 }
 
-// Create display function 
-// Display matches from what the user is typing 
+// Create display function
+// Display matches from what the user is typing
 function displayMatches() {
 	const matchArray = findMatches(this.value, names);
 	const html = matchArray.map(restaurants => {
 		const regex = new RegExp(this.value, 'gi');
-		const restaurantName = restaurants.name.replace(regex, `<span class="h1">${this.value}</span>`); // Highlights the restaurants name 		
+		const restaurantName = restaurants.name.replace(regex, `<span class="h1">${this.value}</span>`); // Highlights the restaurants name
 		return `
 			<li>
 			<span class = "title">${restaurantName}</span>
-			<span class = "address">${restaurants.address_line_1}</span>
-			<span class = "city">${restaurants.city}</span>
-			<span class = "category">${restaurants.category}</span>
+			<address class = "address">${restaurants.address_line_1}</address>
+			<address class = "city">${restaurants.city}</address>
+			<address class = "category">${restaurants.category}</address>
+			<address class = "zip">${restaurants.zip}</address>
 		`;
 	}).join('');
-	recommendations.innerHTML = html;	
-	
+	recommendations.innerHTML = html;
+
 }
 const searchInput = document.querySelector('.typehead');
 const recommendations = document.querySelector('.recommendations');
