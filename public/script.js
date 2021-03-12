cd5a169b26949925ab1641763463cfdb5bbe8382
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2021-03-12 13:30:33 -0500

    Script-v3: Translate original tutorial w/ async

--- public/script.js
+++ public/script.js
@@ -1,43 +1,44 @@
 const endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"; // Adding array to a const variable 
 
-// Empty array to put our restaurant names into 
-const names=[];
+async function windowActions() {
+	const request = await fetch(endpoint) // Fetch request
+	const names = await request.json() // Empty array, this replaces the race condition promise chain
+																		// from the original tutorial
 
-// Using api fetch. Looking into Async/await as a replacement later
-fetch(endpoint)
-	.then(blob => blob.json())
-	.then(data => names.push(...data))
 
-// Run a function to show everything that matches the word being typed in the box
-// The function is going to take this huge array and filter it down into a subset 
-function findMatches(wordToMatch, names) {
-	return names.filter(restaurants => {
-		const regex = new RegExp (wordToMatch, 'gi');
-		return restaurants.name.match(regex) 		
+	function findMatches(wordToMatch, names){
+		return names.filter(restaurants => {
+			const regex = new RegExp (wordToMatch, 'gi');
+			return restaurants.name.match(regex)
+		});
+	}
 
+	function displayMatches(event) {
+		const matchArray = findMatches(event.target.value, names);
+		const html = matchArray.map(restaurants => {
+			const regex = new RegExp(event.target.value, 'gi');
+			const restaurantName = restaurants.name.replace(regex, `<span class="h1">${event.target.value}</span>`); // Highlights the restaurants name 		
+			return `
+				<li>
+				<span class = "title">${restaurantName}</span>
+				<span class = "address">${restaurants.address_line_1}</span>
+				<span class = "city">${restaurants.city}</span>
+				<span class = "category">${restaurants.category}</span>
+				</li>
+			`;
+		}).join('');
+		recommendations.innerHTML = html;	
+		
+	}
+
+	const searchInput = document.querySelector('.typehead');
+	const recommendations = document.querySelector('.recommendations');
+
+	searchInput.addEventListener('change', displayMatches);
+	searchInput.addEventListener('keyup', (evt) => {
+		displayMatches(evt)
 	});
+		
 }
 
-// Create display function 
-// Display matches from what the user is typing 
-function displayMatches() {
-	const matchArray = findMatches(this.value, names);
-	const html = matchArray.map(restaurants => {
-		const regex = new RegExp(this.value, 'gi');
-		const restaurantName = restaurants.name.replace(regex, `<span class="h1">${this.value}</span>`); // Highlights the restaurants name 		
-		return `
-			<li>
-			<span class = "title">${restaurantName}</span>
-			<span class = "address">${restaurants.address_line_1}</span>
-			<span class = "city">${restaurants.city}</span>
-			<span class = "category">${restaurants.category}</span>
-		`;
-	}).join('');
-	recommendations.innerHTML = html;	
-	
-}
-const searchInput = document.querySelector('.typehead');
-const recommendations = document.querySelector('.recommendations');
-
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+window.onload = windowActions;
